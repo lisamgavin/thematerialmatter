130ce2f96699be437290aa0bbd2b6993a5db281c
amy <amy.mccarthy2@mail.dcu.ie>
2012-06-30 18:43:46 +0100

    Basic product list
    I've added a basic product list

--- shop/db/seeds.rb
+++ shop/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+
+Product.create(:title => 'Wool and paper yarn', :description => 'Hand-woven wool yarn with intertwined paper', :price => '15.00')
+Product.create(:title => 'Steel and linen yarn', :description => 'Laser-cut steel yarn with strands of linen', :price => '12.00')
+Product.create(:title => 'Paper and cotton yarn', :description => 'Hand-woven paper yarn with intertwined paper', :price => '11.00')
+Product.create(:title => 'Steel and silk yarn', :description => 'Laser-cut steel yarn intertwined with delicate strands of silk', :price => '14.50')
+Product.create(:title => 'Wool and bronze yarn', :description => 'Hand-woven wool yarn with strands of bronze interwoven', :price => '13.00')
